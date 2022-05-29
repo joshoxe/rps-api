5c915da41941f59e7eddb40b5fc256847cc48678
Josh Oxenham <18144362+joshoxe@users.noreply.github.com>
2022-05-29 13:46:58 +0100

    Make `acl` value a string

--- deployment/main.tf
+++ deployment/main.tf
@@ -25,5 +25,5 @@ resource "aws_s3_bucket" "lambda_bucket" {
 
 resource "aws_s3_bucket_acl" "lambda_bucket_acl" {
   bucket = aws_s3_bucket.lambda_bucket.id
-  acl    = private
+  acl    = "private"
 }

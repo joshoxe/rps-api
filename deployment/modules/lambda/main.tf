8aea2615984db5b301e7e83182ef6f5328fb548f
Josh Oxenham <18144362+joshoxe@users.noreply.github.com>
2022-05-31 15:37:30 +0100

    Fix role reference

--- deployment/modules/lambda/main.tf
+++ deployment/modules/lambda/main.tf
@@ -62,6 +62,6 @@ resource "aws_iam_role_policy_attachment" "lambda_basic_policy" {
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_vpc_execute_policy" {
-  role       = aws_iam_role.lambda.name
+  role       = aws_iam_role.lambda_exec.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
 }

5b2636e0abe9ae9d04bbc9f0563c0398529b7cb1
Josh Oxenham <18144362+joshoxe@users.noreply.github.com>
2022-06-03 19:37:31 +0100

    Try adding serverless.yml

--- deployment/main.tf
+++ deployment/main.tf
@@ -60,7 +60,7 @@ module "lambda_connect_function" {
   output_path        = "${path.root}/../src.zip"
   bucket             = aws_s3_bucket.lambda_bucket.id
   key                = "src.zip"
-  handler            = "onConnect/onConnect.connect"
+  handler            = "onConnect.connect"
   subnet_ids         = module.vpc.intra_subnets
   security_group_ids = [module.vpc.default_security_group_id]
 }
@@ -73,7 +73,7 @@ module "lambda_disconnect_function" {
   output_path        = "${path.root}/../src.zip"
   bucket             = aws_s3_bucket.lambda_bucket.id
   key                = "src.zip"
-  handler            = "onDisconnect/onDiconnect.disconnect"
+  handler            = "onDiconnect.disconnect"
   subnet_ids         = module.vpc.intra_subnets
   security_group_ids = [module.vpc.default_security_group_id]
 }

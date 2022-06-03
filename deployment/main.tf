aeabd9c2bf35c605d649f69fe63b01fecaa99af4
Josh Oxenham <18144362+joshoxe@users.noreply.github.com>
2022-06-03 17:20:47 +0100

    Skip final snapshot in RDS

--- deployment/main.tf
+++ deployment/main.tf
@@ -119,4 +119,5 @@ resource "aws_db_instance" "rds_db" {
   db_subnet_group_name   = aws_db_subnet_group.db_subnet.name
   vpc_security_group_ids = [aws_security_group.rds.id]
   parameter_group_name   = aws_db_parameter_group.rds_params.name
+  skip_final_snapshot    = true
 }

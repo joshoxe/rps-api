17a4eaea188534d6ae5932eee7b8e318d253f0d1
Josh Oxenham <18144362+joshoxe@users.noreply.github.com>
2022-06-03 19:47:20 +0100

    Update handler in TF

--- deployment/main.tf
+++ deployment/main.tf
@@ -60,7 +60,7 @@ module "lambda_connect_function" {
   output_path        = "${path.root}/../src.zip"
   bucket             = aws_s3_bucket.lambda_bucket.id
   key                = "src.zip"
-  handler            = "onConnect.connect"
+  handler            = "./onconnect/onConnect.connect"
   subnet_ids         = module.vpc.intra_subnets
   security_group_ids = [module.vpc.default_security_group_id]
 }
@@ -73,7 +73,7 @@ module "lambda_disconnect_function" {
   output_path        = "${path.root}/../src.zip"
   bucket             = aws_s3_bucket.lambda_bucket.id
   key                = "src.zip"
-  handler            = "onDiconnect.disconnect"
+  handler            = "./ondisconnect/onDisconnect.connect"
   subnet_ids         = module.vpc.intra_subnets
   security_group_ids = [module.vpc.default_security_group_id]
 }

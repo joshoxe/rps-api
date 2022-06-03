191d41a3a5cb184bd19c8d43303e4574e6f9fd64
Josh Oxenham <18144362+joshoxe@users.noreply.github.com>
2022-06-03 19:54:22 +0100

    Update connection handler

--- src/onconnect/onConnect.js
+++ src/onconnect/onConnect.js
@@ -1,14 +1,6 @@
-module.exports.connect = (event, context, callback) => {
-  const response = {
+module.exports.connect = () => {
+  return {
     statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*", // Required for CORS support to work
-    },
-    body: JSON.stringify({
-      message: "Go Serverless v1.0! Your function executed successfully!",
-      input: event,
-    }),
+    body: "Connected",
   };
-
-  callback(null, response);
 };

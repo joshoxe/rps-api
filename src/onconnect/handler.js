40a24d75ff099de4668df074bf91311fcb3a7234
Josh Oxenham <18144362+joshoxe@users.noreply.github.com>
2022-06-08 22:23:34 +0100

    Try logging erroneous event

--- src/onconnect/handler.js
+++ src/onconnect/handler.js
@@ -2,10 +2,18 @@ const PlayerRepository = require('../infra/db/player-repository');
 
 module.exports.connect = async (event, context, callback) => {
   const playerRepository = new PlayerRepository();
-  const newPlayer = {
-    connectionId: event.requestContext.connectionId,
-    roomId: null,
-  };
+  try {
+    const newPlayer = {
+      connectionId: event.requestContext.connectionId,
+      roomId: null,
+    };
+  } catch (err) {
+    callback(null, {
+      statusCode: 200,
+      headers: { 'Access-Control-Allow-Origin': '*' }, // Required for CORS support to work
+      body: JSON.stringify(event),
+    });
+  }
 
   this.headers = {
     'Access-Control-Allow-Origin': '*', // Required for CORS support to work
